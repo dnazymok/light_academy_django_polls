4a387287280a66b894e1304366095625a46c6472
dnazymok <dnazymok@gmail.com>
2021-08-14 20:50:11 +0300

    fix redundant question inline in admin

--- apps/polls/admin.py
+++ apps/polls/admin.py
@@ -3,14 +3,8 @@ from django.contrib import admin
 from .models import Test, Question, Testrun, TestrunQuestion
 
 
-class ChoiceInline(admin.TabularInline):
-    model = Test.questions.through
-    extra = 3
-
-
 class TestAdmin(admin.ModelAdmin):
     list_display = ('test_text', 'test_description', 'pub_date')
-    inlines = [ChoiceInline]
 
 
 class TestrunQuestionInline(admin.TabularInline):
